e022b5253bb1a8e2905d9d8c6b72ed25b3c53ef9
Adrian Muhammad Kuncoro <57836873+dakochan666@users.noreply.github.com>
2023-09-12 18:59:32 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+// bila ada 10 orang, ['a','b','c','d','e','f','g','h','i','j'].
+// orang-orang tsb akan diatur tempat duduk nya berdasarkan urutannya secara vertikal,
+// dan jumlah maksimal kursi per baris vertikal adalah 3.
+// tampilkan siapa saja yg berada di baris 1, 2, dan 3 nya.
+
+// hasil yg benar :
+// Baris 0 :  [ 'a', 'd', 'g', 'j' ]
+// Baris 1 :  [ 'b', 'e', 'h' ]
+// Baris 2 :  [ 'c', 'f', 'i' ]
+
+"use strict";
+
+function generateSeats(row) {
+  // Tulis kode disini
+}
+
+function managePerson(arr, rowSeats) {
+  // Tulis kode disini
+}
+
+function printSeats(seats) {
+  // Tulis kode disini
+}
+
+managePerson(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"], 3);
